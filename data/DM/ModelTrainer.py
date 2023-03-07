fd6db9f95ef09745b71c4282bbac58181e1093c8
cc164 <cc164@hw.ac.uk>
2023-03-07 12:13:01 +0000

    Improves DM model and DialogManager class

--- data/DM/ModelTrainer.py
+++ data/DM/ModelTrainer.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import glob as glob
-from random import random
+from random import random, randrange
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
@@ -14,7 +14,7 @@ args = parser.parse_args()
 
 class TextDataset(Dataset):
 
-    def __init__(self, path=None):
+    def __init__(self, path=None, for_generator=False):
 
         if path == None:
             return
@@ -65,67 +65,133 @@ class TextDataset(Dataset):
 
         longest_length = len(intents[max_index]) - 1
 
-        inputs = torch.zeros([len(intents), longest_length, 17], dtype=torch.float32).cuda()
-        labels = torch.full([len(intents), longest_length, 1], -1, dtype=torch.long).cuda()
+        if(for_generator):
+            inputs = torch.zeros([len(intents), longest_length, 18], dtype=torch.float32).cuda()
 
-        for i in range(len(intents)):
-            current_input = torch.zeros([longest_length, 17], dtype=torch.float32).cuda()
-            current_label = torch.full([longest_length, 1], -1, dtype=torch.long).cuda()
-            
-            for j in range(0, len(intents[i]) - 1):
+            blank_line = torch.zeros(18).cuda()
+            blank_line[0] = 1
+
+            for i in range(len(intents)):
+                current_input = torch.zeros([longest_length, 18], dtype=torch.float32).cuda()
+
+                for j in range(0, longest_length):
+
+                    if (j >= len(intents[i])):
+                        current_input[j] = blank_line.detach().clone()
+                    else:
+                        current_input[j] = self.to_one_hot(intents[i][j][0], intents[i][j][1], for_generator=True)
+
+                inputs[i] = current_input
+
+            self.training = inputs
+
+        else:
+
+            inputs = torch.zeros([len(intents), longest_length, 17], dtype=torch.float32).cuda()
+            labels = torch.full([len(intents), longest_length, 1], -1, dtype=torch.long).cuda()
+
+            for i in range(len(intents)):
+                current_input = torch.zeros([longest_length, 17], dtype=torch.float32).cuda()
+                current_label = torch.full([longest_length, 1], -1, dtype=torch.long).cuda()
                 
-                current_input[j] = self.to_one_hot(intents[i][j][0], intents[i][j][1])
-                current_label[j] = self.to_label(intents[i][j + 1][0], intents[i][j + 1][1])
+                for j in range(0, len(intents[i]) - 1):
+                    
+                    current_input[j] = self.to_one_hot(intents[i][j][0], intents[i][j][1])
+                    current_label[j] = self.to_label(intents[i][j + 1][0], intents[i][j + 1][1])
 
-            inputs[i] = current_input
-            labels[i] = current_label
+                inputs[i] = current_input
+                labels[i] = current_label
 
-        self.training = (inputs, labels)
+            self.training = (inputs, labels)
                 
 
-    def to_one_hot(self, user, input):
+    def to_one_hot(self, user, input, for_generator=False):
 
-        output = torch.zeros(17)
+        if(for_generator):
+            output = torch.zeros(18)
 
-        match user:
-            case "S":
-                output[0] = 1
-            case "U1":
-                output[1] = 1
-            case "U2":
-                output[2] = 1
+            match user:
+                case "S":
+                    output[1] = 1
+                case "U1":
+                    output[2] = 1
+                case "U2":
+                    output[3] = 1
 
-        match input:
-            case "question":
-                output[3] = 1
-            case "options":
-                output[4] = 1
-            case "accept-answer":
-                output[5] = 1
-            case "confirm-agreement":
-                output[6] = 1
-            case "final-answer":
-                output[7] = 1
-            case "confirm-final-answer":
-                output[8] = 1
-            case "offer-answer":
-                output[9] = 1
-            case "offer-to-answer":
-                output[10] = 1
-            case "check-answer":
-                output[11] = 1
-            case "agreement":
-                output[12] = 1
-            case "ask-agreement":
-                output[13] = 1
-            case "chit-chat":
-                output[14] = 1
-            case "reject-option":
-                output[15] = 1
-            case "reject-option-agreement":
-                output[16] = 1
+            match input:
+                case "question":
+                    output[4] = 1
+                case "options":
+                    output[5] = 1
+                case "accept-answer":
+                    output[6] = 1
+                case "confirm-agreement":
+                    output[7] = 1
+                case "final-answer":
+                    output[8] = 1
+                case "confirm-final-answer":
+                    output[9] = 1
+                case "offer-answer":
+                    output[10] = 1
+                case "offer-to-answer":
+                    output[11] = 1
+                case "check-answer":
+                    output[12] = 1
+                case "agreement":
+                    output[13] = 1
+                case "ask-agreement":
+                    output[14] = 1
+                case "chit-chat":
+                    output[15] = 1
+                case "reject-option":
+                    output[16] = 1
+                case "reject-option-agreement":
+                    output[17] = 1
 
-        return output
+            return output
+        else:
+
+            output = torch.zeros(17)
+
+            match user:
+                case "S":
+                    output[0] = 1
+                case "U1":
+                    output[1] = 1
+                case "U2":
+                    output[2] = 1
+
+            match input:
+                case "question":
+                    output[3] = 1
+                case "options":
+                    output[4] = 1
+                case "accept-answer":
+                    output[5] = 1
+                case "confirm-agreement":
+                    output[6] = 1
+                case "final-answer":
+                    output[7] = 1
+                case "confirm-final-answer":
+                    output[8] = 1
+                case "offer-answer":
+                    output[9] = 1
+                case "offer-to-answer":
+                    output[10] = 1
+                case "check-answer":
+                    output[11] = 1
+                case "agreement":
+                    output[12] = 1
+                case "ask-agreement":
+                    output[13] = 1
+                case "chit-chat":
+                    output[14] = 1
+                case "reject-option":
+                    output[15] = 1
+                case "reject-option-agreement":
+                    output[16] = 1
+
+            return output
     
     def to_label(self, user, input):
         if user == "S":
@@ -149,8 +215,12 @@ class TextDataset(Dataset):
 
         return output
 
-    def from_one_hot(self, input):
-        return torch.argmax(input).data
+    def from_one_hot(self, input, threshhold):
+
+        if(input[0][0].data > threshhold):
+            return 0
+
+        return torch.argmax(input[0][1:]).data + 1
 
     def output_to_input(self, input):
         output = torch.zeros(17)
@@ -173,24 +243,25 @@ class RNN(nn.Module):
     def __init__(self):
         super(RNN, self).__init__()
 
-        self.RNN = nn.RNN(input_size=17, hidden_size=64, num_layers=3, bidirectional=False, batch_first=True, dropout=0.15)
+        #self.RNN = nn.RNN(input_size=17, hidden_size=32, num_layers=3, bidirectional=False, batch_first=True, dropout=0.15)
+        self.RNN = nn.LSTM(input_size=17, hidden_size=32, num_layers=2, batch_first=True, dropout=0.15)
 
         self.Linear = nn.Sequential(
             
-            nn.LayerNorm(64),
-            nn.Linear(64, 16),
+            nn.LayerNorm(32),
+            #nn.Linear(32, 16),
             #nn.SiLU(),
             #nn.LayerNorm(32),
             #nn.Linear(32, 16),
-            nn.SiLU(),
-            nn.LayerNorm(16),
-            nn.Linear(16, 5),
+            #nn.SiLU(),
+            #nn.LayerNorm(16),
+            #nn.Linear(16, 5),
             #nn.LogSoftmax(dim=1),
-            nn.Softmax(dim=1),
+            #nn.Softmax(dim=1),
         )
 
         self.Output1 = nn.Sequential(
-            nn.Linear(16, 8),
+            nn.Linear(32, 8),
             nn.LayerNorm(8),
             nn.SiLU(),
             nn.Linear(8, 4),
@@ -199,7 +270,7 @@ class RNN(nn.Module):
         )
 
         self.Output2 = nn.Sequential(
-            nn.Linear(16, 8),
+            nn.Linear(32, 8),
             nn.LayerNorm(8),
             nn.SiLU(),
             nn.Linear(8, 1),
@@ -207,15 +278,17 @@ class RNN(nn.Module):
         )
 
     def initialise_hidden(self):
-        return torch.zeros([3, 64])
+        return torch.zeros([2, 32])
 
-    def forward(self, input, hidden_layer):
-        rnn_out = self.RNN(input, hidden_layer)
+    def forward(self, input, hidden_layer, cell_layer=None):
+        rnn_out = self.RNN(input, (hidden_layer.detach(), cell_layer.detach()))
+        #rnn_out = self.RNN(input, hidden_layer)
         linear_out = self.Linear(rnn_out[0])
-        return linear_out, rnn_out[1]
-        #out1 = self.Output1(linear_out)
-        #out2 = self.Output2(linear_out)
+        #return linear_out, rnn_out[1]
+        out1 = self.Output1(linear_out)
+        out2 = self.Output2(linear_out)
         #return torch.cat((out2, out1), 1).cuda(), rnn_out[1]
+        return torch.cat((out2, out1), 1).cuda(), rnn_out[1][0], rnn_out[1][1]
 
 class CustomLossFunction():
     def __init__(self, labels, formatter):
@@ -239,12 +312,13 @@ class CustomLossFunction():
 
         self.loss_weights = nn.functional.normalize(self.loss_weights, dim=0).cuda()
 
-
     def calculate(self, output, expected):
         one_hot = self.formatter.to_one_hot_label(expected)
 
         difference = torch.subtract(output, one_hot)
-        difference = torch.multiply(difference, self.loss_weights)
+        if expected == 0:
+            difference[1:] = torch.multiply(difference[1:], 0)
+        #difference = torch.multiply(difference, self.loss_weights[expected])
         difference = torch.multiply(difference, difference)
 
         return torch.mean(difference)
@@ -252,7 +326,7 @@ class CustomLossFunction():
 def train(model, epochs, learning_rate, training_data):
 
     data_count = training_data.__len__()
-    validation_count = 10
+    validation_count = 20
     input_data, input_labels = training_data.get_dataset()
 
     optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -261,6 +335,18 @@ def train(model, epochs, learning_rate, training_data):
 
     input_data = input_data.cuda()
     input_labels = input_labels.squeeze(2).cuda()
+
+    swapped_input_data = input_data.detach().clone()
+
+    for i in range(0, swapped_input_data.shape[0]):
+        for j in range(0, swapped_input_data.shape[1]):
+            if input_data[i][j][1] == 1:
+                input_data[i][j][1] = 0
+                input_data[i][j][2] = 1
+            else:
+                if input_data[i][j][2] == 1:
+                    input_data[i][j][1] = 1
+                    input_data[i][j][2] = 0
     
     for i in range(0, epochs):
 
@@ -270,19 +356,12 @@ def train(model, epochs, learning_rate, training_data):
 
         permutation_indexes = torch.randperm(input_data.size()[0] - validation_count).type(torch.int64).cuda()
 
-        current_batch = torch.index_select(input_data, 0, permutation_indexes).cuda()
-        current_labels = torch.index_select(input_labels, 0, permutation_indexes).cuda()
+        if random() > 0.5:
+            current_batch = torch.index_select(input_data, 0, permutation_indexes).cuda()
+        else:
+            current_batch = torch.index_select(swapped_input_data, 0, permutation_indexes).cuda()
 
-        for j in range(0, current_batch.shape[0]):
-            if random() > 0.5:
-                for k in range(0, current_batch[j].shape[0]):
-                    if current_batch[j][k][1] == 1:
-                        current_batch[j][k][1] = 0
-                        current_batch[j][k][2] = 1
-                    else:
-                        if current_batch[j][k][2] == 1:
-                            current_batch[j][k][1] = 1
-                            current_batch[j][k][2] = 0
+        current_labels = torch.index_select(input_labels, 0, permutation_indexes).cuda()
 
         for j in range(0, input_data.shape[0]):
 
@@ -291,6 +370,7 @@ def train(model, epochs, learning_rate, training_data):
                 model.train()
 
                 hidden_layer = model.initialise_hidden().cuda()
+                cell_layer = model.initialise_hidden().cuda()
 
                 current_loss = 0
 
@@ -299,12 +379,14 @@ def train(model, epochs, learning_rate, training_data):
                     if current_labels[j][k].item() == -1:
                         break
                     
-                    output, hidden_layer = model(current_batch[j][k].unsqueeze(0), hidden_layer)
+                    #output, hidden_layer = model(current_batch[j][k].unsqueeze(0), hidden_layer)
+                    output, hidden_layer, cell_layer = model(current_batch[j][k].unsqueeze(0), hidden_layer, cell_layer)
 
                     current_loss+= criterion.calculate(output, current_labels[j][k].unsqueeze(0))
 
                     if current_batch[j][k][0] == 1 and ((k + 1) == current_batch[j].shape[0] or current_batch[j][k + 1][0] == 0 or current_labels[j][k + 1].item() == -1):
-                        output, hidden_layer = model(training_data.output_to_input(training_data.from_one_hot(output)).unsqueeze(0).cuda(), hidden_layer)
+                        #output, hidden_layer = model(training_data.output_to_input(training_data.from_one_hot(output)).unsqueeze(0).cuda(), hidden_layer)
+                        output, hidden_layer, cell_layer = model(training_data.output_to_input(training_data.from_one_hot(output)).unsqueeze(0).cuda(), hidden_layer, cell_layer)
                         current_loss+= criterion.calculate(output, training_data.to_label("", ""))
 
                 if current_loss > 0:
@@ -317,13 +399,15 @@ def train(model, epochs, learning_rate, training_data):
                 model.eval()
 
                 hidden_layer = model.initialise_hidden().cuda()
+                cell_layer = model.initialise_hidden().cuda()
 
                 for k in range(0, input_data[j].shape[0]):
 
                     if input_labels[j][k].item() == -1:
                         break
 
-                    output, hidden_layer = model(input_data[j][k].unsqueeze(0), hidden_layer)
+                    #output, hidden_layer = model(input_data[j][k].unsqueeze(0), hidden_layer)
+                    output, hidden_layer, cell_layer = model(input_data[j][k].unsqueeze(0), hidden_layer, cell_layer)
 
                     validation_total+= 1
 
@@ -346,7 +430,8 @@ def main():
 
     model = RNN().cuda()
 
-    train(model, 60, 0.00005, training_data)
+    train(model, 30, 0.0005, training_data)
+    #train(model, 60, 0.00005, training_data)
 
     torch.save(model.state_dict(), os.path.join(current_path, "Model.pth"))
 
